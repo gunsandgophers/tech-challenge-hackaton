c880844350afbde400a485fbd78e54c6a9362bc8
Gabriel Miranda Carvalho <gabrielm.car@gmail.com>
2025-01-31 01:49:03 -0300

    feat: list in video repository

--- internal/infra/repositories/video_repository_db.go
+++ internal/infra/repositories/video_repository_db.go
@@ -1,7 +1,9 @@
 package repositories
 
 import (
+	"log"
 	"tech-challenge-hackaton/internal/application/entities"
+	"tech-challenge-hackaton/internal/application/vo"
 	"tech-challenge-hackaton/internal/infra/database"
 )
 
@@ -15,7 +17,7 @@ func NewVideoRepositoryDB(conn database.ConnectionDB) *VideoRepositoryDB {
 
 func (r *VideoRepositoryDB) Insert(video *entities.Video) error {
 	sql := `
-	INSERT INTO videos(id, user_id, filename, status, mimeType)
+	INSERT INTO videos(id, user_id, filename, status, mime_type)
 	VALUES ($1, $2, $3, $4, $5);
 	`
 	return r.conn.Exec(
@@ -25,5 +27,65 @@ func (r *VideoRepositoryDB) Insert(video *entities.Video) error {
 		video.GetFilename(),
 		video.GetStatus().String(),
 		video.GetMimeType().String(),
+	)
+}
+
+func (r *VideoRepositoryDB) ListByUserID(userID string) ([]*entities.Video, error) {
+	sql := `
+	SELECT 
+		id,
+		user_id,
+		filename,
+		status,
+		mime_type,
+	FROM public.videos
+	WHERE
+		user_id = $1
+	`
+	rows, err := r.conn.Query(sql, userID)
+	if err != nil {
+		return nil, err
+	}
+	return r.toDomainList(rows), nil
+}
+
+func (r *VideoRepositoryDB) toDomainList(rows database.RowsDB) []*entities.Video {
+	var videos []*entities.Video
+	for rows.Next() {
+		if v, err := r.toDomain(rows); err == nil {
+			videos = append(videos, v)
+		} else {
+			log.Println(err)
+		}
+	}
+	return videos
+}
+
+func (r *VideoRepositoryDB) toDomain(row database.RowDB) (*entities.Video, error) {
+	var (
+		id       string
+		userID   string
+		filename string
+		status   string
+		mimeType string
+	)
+
+	err := row.Scan(
+		&id,
+		&userID,
+		&filename,
+		&status,
+		&mimeType,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return entities.RestoreVideo(
+		id,
+		userID,
+		vo.VideoStatus(status),
+		filename,
+		vo.MIMEType(mimeType),
 	)
 }
--- migrations/20250119194911_videos.up.sql
+++ migrations/20250119194911_videos.up.sql
@@ -3,7 +3,7 @@ CREATE TABLE videos
     id uuid NOT NULL,
     user_id uuid NOT NULL,
     status VARCHAR(20) NOT NULL DEFAULT 'AWAITING',
-    mimeType VARCHAR(50) NOT NULL,
+    mime_type VARCHAR(50) NOT NULL,
     filename VARCHAR(255) NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
